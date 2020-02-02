6bd4b80673e27be4319852ed407924fb72f42c31
LE Minh-Huy <m.le@numendo.com>
2020-02-02 20:26:04 +0100

    add angular

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const serveStatic = require('serve-static')
+const mime = require('mime');
 const SRC_PATH = '/src';
-const HTML_PATH = __dirname + SRC_PATH + '/html';
+const HTML_PATH = __dirname + '/dist';
 const BDD_PATH = __dirname + SRC_PATH + '/data';
 var bddService = require('./src/services/bdd.service');
 
-router.get('/',function(req,res){
+app.get('/',function(req,res){
   bddService.appendData(BDD_PATH + '/oracle_low_cost.bdd',  'oook');
 
   res.sendFile(path.join(HTML_PATH + '/index.html'));
 });
 
+
+// ---- SERVE APLICATION PATHS ---- //
+app.all('*', function (req, res) {
+  res.sendFile(path.join(HTML_PATH+req.url));
+});
 
 app.use('/', router);
 app.listen(process.env.port || 3000);
